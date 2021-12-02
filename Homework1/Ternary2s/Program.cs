8a5cfe4693ab12b305482684b19774b369d06695
VytasR <vytarink@gmail.com>
2021-12-02 09:05:58 +0200

    Homework1, app Ternary2s created method ConvertDecimalToTernary()

--- Homework1/Ternary2s/Program.cs
+++ Homework1/Ternary2s/Program.cs
@@ -8,6 +8,26 @@ namespace Ternary2s
 {
     internal class Program
     {
+
+        // This method inputs integer and converts it to ternary numeral system. Returns a string.
+        static string ConvertDecimalToTernary (int number)
+        {
+            if (number == 0) return "0";
+
+            var ternaryRepresentation = new StringBuilder(5);
+            int quotient = Math.Abs(number);
+
+            while (quotient != 0)
+            {
+                ternaryRepresentation.Insert(0, Convert.ToString(quotient % 3));
+                quotient /= 3;
+            }
+
+            if (number < 0) ternaryRepresentation.Insert(0, "-");
+
+            return ternaryRepresentation.ToString();
+        }
+
         static void Main(string[] args)
         {
         }
