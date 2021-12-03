768f79bc88c60c1bf7d9ef11e79b7b7343c8e3e4
VytasR <vytarink@gmail.com>
2021-12-03 10:14:53 +0200

    Homework1, app ArraySummator, method InputIntArray() created

--- Homework1/ArraySummator/Program.cs
+++ Homework1/ArraySummator/Program.cs
@@ -13,6 +13,34 @@ namespace ArraySummator
          * the sum of the array elements located between the smallest element in the array (the leftmost element if there are several)
          * and the largest element (the rightmost element if there are several).
          */
+
+        // Reads a single-dimensional array of integers from console. Array must contain at least two elements.
+        static int[] InputIntArray()
+        {
+            var arrayLength = 0;
+            var isArrayLengthAtLeastTwo = false;
+
+            while (!isArrayLengthAtLeastTwo)
+            {
+                Console.Write("Please enter array length (min 2): ");
+                arrayLength = Int32.Parse(Console.ReadLine());
+
+                if (arrayLength >= 2)
+                    isArrayLengthAtLeastTwo = true;
+                else
+                    Console.WriteLine("Array size error.");
+            }
+
+            int[] intArray = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"Please enter array element No. {i}: ");
+                intArray[i] = Int32.Parse(Console.ReadLine());
+            }
+            return intArray;
+        }
+
         static void Main(string[] args)
         {
         }
