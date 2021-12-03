76dcbb9a3d01c8b779e650a93c19ed6f7450d21d
VytasR <vytarink@gmail.com>
2021-12-03 09:27:16 +0200

    Homewrok1, app ISBNCalculator, program decription comment added

--- Homework1/ISBNCalculator/Program.cs
+++ Homework1/ISBNCalculator/Program.cs
@@ -8,6 +8,11 @@ namespace ISBNCalculator
 {
     internal class Program
     {
+        /*
+         * This program reads first 9 digits of ISBN from console,
+         * calculates the check digit and writes to console the full ISBN number.
+         */
+
         static void Main(string[] args)
         {
         }
