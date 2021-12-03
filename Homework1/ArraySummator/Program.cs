b9ba717052cdc2df992d21797b1cd5ee74a83ba6
VytasR <vytarink@gmail.com>
2021-12-03 10:10:05 +0200

    Homework1, app ArraySummator, program description comment added

--- Homework1/ArraySummator/Program.cs
+++ Homework1/ArraySummator/Program.cs
@@ -8,6 +8,11 @@ namespace ArraySummator
 {
     internal class Program
     {
+        /*
+         * This application asks the user to enter a numeric array (of int type). Then calculates and outputs 
+         * the sum of the array elements located between the smallest element in the array (the leftmost element if there are several)
+         * and the largest element (the rightmost element if there are several).
+         */
         static void Main(string[] args)
         {
         }
