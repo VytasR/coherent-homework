2d94a4498fa7cf04c49b0d410cc3f506bcf65eb8
VytasR <vytarink@gmail.com>
2021-12-13 12:18:09 +0200

    Homework2, app DiagonalMX, class DiagonalMatrix constructor created

--- Homework2/DiagonalMX/DiagonalMX/DiagonalMatrix.cs
+++ Homework2/DiagonalMX/DiagonalMX/DiagonalMatrix.cs
@@ -10,5 +10,21 @@ namespace DiagonalMX
     {
         private int[] _diagonalElements;
         private int Size { get; }
+
+        public DiagonalMatrix (params int[] list)
+        {            
+            if (list != null)
+            {
+                int matrixSize = list.Length;
+                _diagonalElements = new int[matrixSize];
+                Array.Copy(list, _diagonalElements, matrixSize);
+                Size = matrixSize;
+            }
+            else
+            {
+                _diagonalElements = new int[0];
+                Size = 0;
+            }
+        }
     }
 }
