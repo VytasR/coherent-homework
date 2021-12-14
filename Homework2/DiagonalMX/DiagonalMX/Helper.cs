b62f926207490491ce6aafff762c9d2b8514d108
VytasR <vytarink@gmail.com>
2021-12-14 18:16:49 +0200

    Homework2, app DiagonalMX, method Add() created in class Helper

--- Homework2/DiagonalMX/DiagonalMX/Helper.cs
+++ Homework2/DiagonalMX/DiagonalMX/Helper.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace DiagonalMX
 {
-    internal class Helper
+    internal static class Helper
     {
+                       
+        // Inputs two diagonal matrices. Adds elements of the matrices and returns a new diagonal matrix.
+        // Returns null if both matrices are null. If only one is null then returns the other matrix.
+        // If the dimensions of the matrix do not match, the smaller matrix is padded with zeros.
+        public static DiagonalMatrix Add(this DiagonalMatrix thisMatrix, DiagonalMatrix anotherMatrix)
+        {            
+            if (thisMatrix == null && anotherMatrix == null)
+            {
+                return null;
+            }
+            
+            int newMatrixSize;
+
+            if (anotherMatrix == null)
+            {
+                newMatrixSize = thisMatrix.Size;
+                anotherMatrix = new DiagonalMatrix();
+            }
+            else if (thisMatrix == null)
+            {
+                newMatrixSize = anotherMatrix.Size;
+                thisMatrix = new DiagonalMatrix();
+            }
+            else
+            {
+                newMatrixSize = thisMatrix.Size > anotherMatrix.Size ? thisMatrix.Size : anotherMatrix.Size;
+            }
+            
+            int[] newMatrixElements = new int[newMatrixSize];
+
+            for (int i = 0; i < newMatrixSize; i++)
+            {
+                newMatrixElements[i] = thisMatrix[i, i] + anotherMatrix[i, i];
+            }
+
+            return new DiagonalMatrix(newMatrixElements);
+        }
     }
 }
