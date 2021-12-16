798dc669a1105cb36cc627fdb928677a4cf506e9
VytasR <vytarink@gmail.com>
2021-12-16 12:04:03 +0200

    Homework2, app DiagonalMX, demo added to Program Main()

--- Homework2/DiagonalMX/DiagonalMX/Program.cs
+++ Homework2/DiagonalMX/DiagonalMX/Program.cs
@@ -10,6 +10,21 @@ namespace DiagonalMX
     {
         static void Main(string[] args)
         {
+            var diagonalMatrix1 = new DiagonalMatrix(11, 123456789, 33, -123456789);
+            var diagonalMatrix2 = new DiagonalMatrix(-11, -123456789, -33, 123456789);
+            var diagonalMatrix3 = Helper.Add(diagonalMatrix1, diagonalMatrix2);
+
+            Console.WriteLine($"diagonalMatrix1:\n{diagonalMatrix1}");
+            Console.WriteLine();
+            Console.WriteLine($"diagonalMatrix2:\n{diagonalMatrix2}");
+            Console.WriteLine();
+            Console.WriteLine($"Helper.Add(diagonalMatrix1, diagonalMatrix2):\n{diagonalMatrix3}");
+            Console.WriteLine();
+            Console.WriteLine($"diagonalMatrix1.Equals(diagonalMatrix3): {diagonalMatrix1.Equals(diagonalMatrix3)}");
+            Console.WriteLine($"diagonalMatrix1.Equals(diagonalMatrix1): {diagonalMatrix1.Equals(diagonalMatrix1)}");
+            Console.WriteLine($"diagonalMatrix1.Track(): {diagonalMatrix1.Track()}");
+
+            Console.ReadKey();
         }
     }
 }
