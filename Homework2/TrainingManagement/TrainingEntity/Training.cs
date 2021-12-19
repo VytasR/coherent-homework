e9b85d12df1ac98201b5261d640e44576b9126b9
VytasR <vytarink@gmail.com>
2021-12-19 10:41:45 +0200

    Homework2, app TrainingManagement, class Training fields defined

--- Homework2/TrainingManagement/TrainingEntity/Training.cs
+++ Homework2/TrainingManagement/TrainingEntity/Training.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingManagement.LessonEntities;
 
 namespace TrainingManagement
 {
     internal class Training
     {
+        public string Description { get; set; }
+        private const int MAX_NUMBER_OF_LESSONS = 10;
+        private Lesson[] lessons;
+
     }
 }
