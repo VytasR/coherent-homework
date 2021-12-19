4f8d3aac554157da9ff5c2ebc250e027b69f5b43
VytasR <vytarink@gmail.com>
2021-12-19 11:23:56 +0200

    Homework2, app TrainingManagement, method Add() created in class Training

--- Homework2/TrainingManagement/TrainingEntity/Training.cs
+++ Homework2/TrainingManagement/TrainingEntity/Training.cs
@@ -12,10 +12,24 @@ namespace TrainingManagement
         public string Description { get; set; }
         private const int MAX_NUMBER_OF_LESSONS = 10;
         private Lesson[] lessons;
+        private int _lessonsIncluded = 0;
 
         public Training (string description)
         {
             Description = description;
+        }
+
+        public void Add (Lesson lesson)
+        {
+            if (_lessonsIncluded < MAX_NUMBER_OF_LESSONS)
+            {
+                lessons[_lessonsIncluded] = lesson;
+            } 
+            else
+            {
+                throw new Exception("Max number of lessons exceeded.");
+            }
+            
         }
     }
 }
