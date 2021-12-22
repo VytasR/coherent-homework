21691393ed384ec7cf1394751f2014db3bc4b86f
VytasR <vytarink@gmail.com>
2021-12-22 11:15:00 +0200

    Homework2, app TrainingManagement, class Training, lesson array replaced with List

--- Homework2/TrainingManagement/TrainingEntity/Training.cs
+++ Homework2/TrainingManagement/TrainingEntity/Training.cs
@@ -9,51 +9,28 @@ namespace TrainingManagement
 {
     internal class Training
     {
-        public string Description { get; set; }
-        private const int MAX_NUMBER_OF_LESSONS = 10;
-        private Lesson[] Lessons;
-        private int _numberOfLessons;
+        public string Description { get; set; }        
+        private List<Lesson> Lessons;        
 
         public Training (string description)
         {
             Description = description;
-            Lessons = new Lesson[MAX_NUMBER_OF_LESSONS];
-            _numberOfLessons = 0;
-        }
-
-        public int NumberOfLessons => _numberOfLessons;
-
-        public Lesson this[int index]
-        {
-            get
-            {
-                if (index < MAX_NUMBER_OF_LESSONS && index >= 0)
-                {
-                    return Lessons[index];
-                } 
-                else
-                {
-                    return null;
-                }
-            }
+            Lessons = new List<Lesson>();            
         }
 
         public void Add (Lesson lesson)
         {
-            if (_numberOfLessons < MAX_NUMBER_OF_LESSONS)
-            {
-                Lessons[_numberOfLessons] = lesson;
-                _numberOfLessons++;
-            } 
-            else
-            {
-                throw new Exception("Max number of lessons exceeded.");
-            }            
+            Lessons.Add(lesson);            
         }
 
         // Returns true if the training contains only practical lessons.
         public bool IsPractical()
         {            
+            if (!Lessons.Any())
+            {
+                return false;
+            }
+
             foreach (var lesson in Lessons)
             {
                 if (lesson is Lecture)
@@ -69,23 +46,25 @@ namespace TrainingManagement
         {
             Training other = (Training) this.MemberwiseClone();
             other.Description = String.Copy(Description);
-            for (int index = 0; index < _numberOfLessons; index++)
+            for (int index = 0; index < Lessons.Count(); index++)
             {
-                if (this[index] is Lecture)
+                if (Lessons[index] is Lecture)
                 {
-                    string description = String.Copy(this[index].Description);
-                    string topic = String.Copy(((Lecture) this[index]).Topic);
+                    string description = String.Copy(Lessons[index].Description);
+                    string topic = String.Copy(((Lecture) Lessons[index]).Topic);
                     other.Add(new Lecture(description, topic));
                 }
-                else if (this[index] is PracticalLesson)
+                else if (Lessons[index] is PracticalLesson)
                 {
-                    string description = String.Copy(this[index].Description);
-                    string linkToTaskCondition = String.Copy(((PracticalLesson)this[index]).LinkToTaskCondition);
-                    string linkToTaskSolution = String.Copy(((PracticalLesson)this[index]).LinkToTaskCondition);
+                    string description = String.Copy(Lessons[index].Description);
+                    string linkToTaskCondition = String.Copy(((PracticalLesson) Lessons[index]).LinkToTaskCondition);
+                    string linkToTaskSolution = String.Copy(((PracticalLesson) Lessons[index]).LinkToTaskCondition);
                     other.Add(new PracticalLesson(description, linkToTaskCondition, linkToTaskSolution));
                 }
             }
+
             return other;
         }
+                
     }
 }
