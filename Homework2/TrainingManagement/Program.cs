8c82a283c8e10d2da3781dbafdab51094aea3886
VytasR <vytarink@gmail.com>
2021-12-22 11:51:23 +0200

    Homework2, app TrainingManagement, demo added to Main()

--- Homework2/TrainingManagement/Program.cs
+++ Homework2/TrainingManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingManagement.LessonEntities;
 
 namespace TrainingManagement
 {
@@ -10,6 +11,22 @@ namespace TrainingManagement
     {
         static void Main(string[] args)
         {
+            var lecture1 = new Lecture("C# types and variables", String.Empty);
+            var lecture2 = new Lecture("C# program structure", String.Empty);
+
+            var practicalLesson1 = new PracticalLesson("Building loops", String.Empty, String.Empty);
+            var practicalLesson2 = new PracticalLesson("Conditional statements", String.Empty, String.Empty);
+
+            var training1 = new Training("Introduction to C#");
+            training1.Add(lecture1);
+            training1.Add(lecture2);
+            training1.Add(practicalLesson1);
+            training1.Add(practicalLesson2);
+
+            Console.WriteLine(training1);
+
+            Console.ReadKey();
+
         }
     }
 }
