876f077ba6aec2e7e454cff4c7279e66695f2034
VytasR <vytarink@gmail.com>
2021-12-29 16:55:48 +0200

    HW3, app Piano, enumerations created

--- Homework3/Piano/KeyStructure/Key.cs
+++ Homework3/Piano/KeyStructure/Key.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace Piano.KeyEntity
 {
+
+    public enum Octave
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth,
+        Sixth,
+        Seventh
+    }
+
+    public enum Note
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G
+    }
+
+    public enum Accidental
+    {
+        NoSign,
+        Sharp,
+        Flat
+    }
+
     struct Key
     {
     }
