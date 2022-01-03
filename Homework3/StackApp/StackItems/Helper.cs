fd53c1f4ffc4cceb3784783465390642fc94b6d3
VytasR <vytarink@gmail.com>
2022-01-03 11:11:04 +0200

    HW3, StackApp, class Helper method Reverse() created

--- Homework3/StackApp/StackItems/Helper.cs
+++ Homework3/StackApp/StackItems/Helper.cs
@@ -8,5 +8,17 @@ namespace StackApp.StackItems
 {
     internal static class Helper
     {
+        // Returns a new stack in which the elements of the original parameter stack are in reverse order.
+        public static IStack<T> Reverse<T>(IStack<T> stack) where T : struct
+        {
+            IStack<T> reverseStack = new Stack<T>();
+
+            while (!stack.IsEmpty())
+            {
+                reverseStack.Push(stack.Pop());
+            }
+
+            return reverseStack;
+        }
     }
 }
