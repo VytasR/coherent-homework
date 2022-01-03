328070a2687bdfd31775ed04b53b510e26e4981e
VytasR <vytarink@gmail.com>
2022-01-03 11:05:42 +0200

    HW3, StackApp, demo created in Program Main()

--- Homework3/StackApp/Program.cs
+++ Homework3/StackApp/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using StackApp.StackItems;
+using System;
+//using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace StackApp
     {
         static void Main(string[] args)
         {
+            IStack<int> intStack1 = new Stack<int>();
+            Console.WriteLine($"The stack is empty: {intStack1.IsEmpty()}");
+            intStack1.Push(1);
+            intStack1.Push(2);
+            intStack1.Push(3);
+            intStack1.Push(4);
+            intStack1.Push(5);
+            intStack1.Push(6);
+            Console.WriteLine($"The stack is empty: {intStack1.IsEmpty()}");
+            Console.WriteLine();
+            Console.WriteLine("Integer stack items:");
+            while (!intStack1.IsEmpty())
+            {
+                Console.WriteLine(intStack1.Pop());
+            }
+
+            var floatStack1 = new Stack<float>();
+            floatStack1.Push(25.6f);
+            floatStack1.Push(11.58f);
+            floatStack1.Push(0.689f);
+            floatStack1.Push(-48.654f);
+            Console.WriteLine();
+            Console.WriteLine("Float stack items:");
+            while (!floatStack1.IsEmpty())
+            {
+                Console.WriteLine(floatStack1.Pop());
+            }
         }
     }
 }
