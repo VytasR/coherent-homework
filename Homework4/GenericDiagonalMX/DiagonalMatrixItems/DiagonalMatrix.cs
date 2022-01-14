d03cfa1d01c2319be2a45d4890a345e84e79061b
VytasR <vytarink@gmail.com>
2022-01-14 10:24:24 +0200

    HW4, app GenericDiagonalMX, indexer created in class DiagonalMatrix

--- Homework4/GenericDiagonalMX/DiagonalMatrixItems/DiagonalMatrix.cs
+++ Homework4/GenericDiagonalMX/DiagonalMatrixItems/DiagonalMatrix.cs
@@ -23,5 +23,36 @@ namespace GenericDiagonalMX.DiagonalMatrixItems
                 _diagonalElements = new T[Size];
             }
         }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i <= 0 || i >= Size || j <= 0 || j >= Size)                
+                {                    
+                    throw new IndexOutOfRangeException("Index out of bounds of the diagonal matrix.");
+                }
+                else if (i == j)
+                {
+                    return _diagonalElements[i];
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+
+            set
+            {
+                if (i <= 0 || i >= Size || j <= 0 || j >= Size)
+                {
+                    throw new IndexOutOfRangeException("Index out of bounds of the diagonal matrix.");
+                }
+                else if (i == j)
+                {
+                   _diagonalElements[i] = value;
+                }
+            }
+        }
     }
 }
