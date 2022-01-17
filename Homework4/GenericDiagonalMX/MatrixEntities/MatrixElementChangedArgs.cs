a10f4a8e5bb070a787f37c1329892d89f5a49631
VytasR <vytarink@gmail.com>
2022-01-17 18:51:51 +0200

    HW4, app GenericDiagonalMX, namespaces modified

---
+++ Homework4/GenericDiagonalMX/MatrixEntities/MatrixElementChangedArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDiagonalMX.MatrixEntities
+{
+    internal class MatrixElementChangedArgs<T> : EventArgs
+    {
+        // Instances of this class contain diagonal matrix ElementChanged event data.
+
+        public int Row { get; }
+        public int Column { get; }
+        public T OldValue { get; }
+        public T NewValue { get; }
+
+        public MatrixElementChangedArgs(int row, int column, T oldValue, T newValue)
+        {
+            Row = row;
+            Column = column;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
--- Homework4/GenericDiagonalMX/Program.cs
+++ Homework4/GenericDiagonalMX/Program.cs
@@ -1,4 +1,4 @@
-﻿using GenericDiagonalMX.DiagonalMatrixItems;
+﻿using GenericDiagonalMX.MatrixEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
