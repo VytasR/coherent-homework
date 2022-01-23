302e1be896a74624a5da280022424e0a89a1ed53
VytasR <vytarink@gmail.com>
2022-01-23 12:56:35 +0200

    HW5, SparseMatrixApp, class ByCoordinates created

---
+++ Homework5/SparseMatrixApp/SparseMatrixEntities/ByCoordinates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparseMatrixApp.SparseMatrixEntities
+{
+    internal class ByCoordinates : IComparer<Coordinates>
+    {
+        public int Compare(Coordinates x, Coordinates y)
+        {
+            if (x.Column.CompareTo(y.Column) != 0)
+            {
+                return x.Column.CompareTo(y.Column);
+            }
+            else if (x.Row.CompareTo(y.Row) != 0)
+            {
+                return x.Row.CompareTo(y.Row);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
