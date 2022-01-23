2ac40e7e6b5f753391b1a6669fd12868c78243f3
VytasR <vytarink@gmail.com>
2022-01-23 07:45:06 +0200

    HW5, SparseMatrixApp, namespace renamed

--- Homework5/SparseMatrixApp/SparseMatrixEntities/SparseMatrix.cs
+++ Homework5/SparseMatrixApp/SparseMatrixEntities/SparseMatrix.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SparseMatrixApp.SparseMatrixEntity
+namespace SparseMatrixApp.SparseMatrixEntities
 {
     internal class SparseMatrix
     {
