9ff75cc9bb3fae6a75bd9945e60560c2f853b69f
VytasR <vytarink@gmail.com>
2022-01-23 13:23:45 +0200

    HW5, SpatseMatrixApp, demo added in Program Main()

--- Homework5/SparseMatrixApp/Program.cs
+++ Homework5/SparseMatrixApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SparseMatrixApp.SparseMatrixEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +9,39 @@ namespace SparseMatrixApp
 {
     internal class Program
     {
+        // This program demonstrates usage of SparseMatrix class
         static void Main(string[] args)
-        {            
-            int numberOfRows = 100;
-            int numberOfColumns = 200;
-            SortedList<int, int>[] _items = new SortedList<int, int>[numberOfRows];
-            for (int row = 0; row < numberOfRows; row++)
-            {
-                _items[row] = new SortedList<int, int>();
-            }
-            _items[5].Add(10, 1256);
-            _items[5].Add(55, -899);
-            _items[5].Add(37, 59845);
+        {
+            var matrix1 = new SparseMatrix(3, 4);
+            matrix1[0, 3] = -985;
+            matrix1[2, 3] = -985;
+            matrix1[0, 2] = -985;
+            matrix1[1, 2] = 3;
+            matrix1[2, 1] = 586;
+            matrix1[0, 3] = 0;
+            matrix1[1, 0] = 555;
+            Console.WriteLine($"Matrix items:\n{matrix1}");
 
-            for (int row = 0; row < numberOfRows; row++)
+            Console.WriteLine();
+            Console.WriteLine($"There are {matrix1.GetCount(0)} elements with value 0");
+            Console.WriteLine($"There are {matrix1.GetCount(-985)} elements with value -985");
+            Console.WriteLine($"There are {matrix1.GetCount(-1)} elements with value -1");
+
+            Console.WriteLine();
+            Console.WriteLine("Non zero items in matrix: ");
+            foreach (var item in matrix1.GetNonZeroElements())
             {
-                foreach (var item in _items[row])
-                {
-                    Console.WriteLine($"Row {row}, Column {item.Key}, Value {item.Value}");
-                }
+                Console.WriteLine($"({item.Item1}, {item.Item2}) = {item.Item3}");
             }
 
+            Console.WriteLine();
+            Console.WriteLine("List of all matrix items: ");
+            int itemCount = 0;
+            foreach (var item in matrix1)
+            {
+                Console.WriteLine($"item {itemCount} = {item}");
+                itemCount++;
+            }
         }
     }
 }
