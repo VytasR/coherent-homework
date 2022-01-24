2e0eb3d49ce38a4438abc3dd53704ccb4c097385
VytasR <vytarink@gmail.com>
2022-01-24 20:58:23 +0200

    HW5, BookCatalogApp, class Book implemented

--- Homework5/BookCatalogApp/BookEntities/Book.cs
+++ Homework5/BookCatalogApp/BookEntities/Book.cs
@@ -8,5 +8,43 @@ namespace BookCatalogApp.BookEntities
 {
     internal class Book
     {
+        private string _title;
+        public string Title 
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid book title. Must be not null and not empty.");
+                }
+                _title = value;
+            } 
+        }
+        public string PublicationDate { get; set; }
+        public HashSet<string> Authors { get; set; }
+
+        public Book(string title)
+        {
+            if (String.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Invalid book title. Must be not null and not empty.");
+            }
+            Title = title;
+            Authors = new HashSet<string>();
+        }
+
+        public Book(string title, string publicationDate) : this(title)
+        {
+            PublicationDate = publicationDate;            
+        }
+
+        public bool AddAuthor(string author)
+        {
+            return Authors.Add(author);
+        }
     }
 }
