38ab23292426c2c440230e7cc79c6cdf81daaa71
VytasR <vytarink@gmail.com>
2022-01-25 15:12:26 +0200

    HW5, BookCatalogApp, method FormatISBN() created in class Catalog

--- Homework5/BookCatalogApp/BookEntities/Catalog.cs
+++ Homework5/BookCatalogApp/BookEntities/Catalog.cs
@@ -18,25 +18,8 @@ namespace BookCatalogApp.BookEntities
 
         public void AddBook(string iSBN, Book newBook)
         {
-            var newISBN = String.Empty;
-            var digitsOnly = new Regex(@"^\d{13}$");
-            var digitsWithHyphens = new Regex(@"^\d{3}-\d-\d{2}-\d{6}-\d$");
-            var matchDigitsOnly = digitsOnly.Match(iSBN);
-            var matchDigitsWithHyphens = digitsWithHyphens.Match(iSBN);
-
-            if (matchDigitsOnly.Success)
-            {
-                newISBN = matchDigitsOnly.Value;
-            }
-            else if (matchDigitsWithHyphens.Success)
-            {
-                newISBN = matchDigitsWithHyphens.Value.Replace("-", String.Empty);
-            }
-            else
-            {
-                throw new ArgumentException("Invalid ISBN number format.");
-            }
-
+            var newISBN = FormatISBN(iSBN);            
+                       
             if (!books.ContainsKey(newISBN))
             {
                 books.Add(newISBN, newBook);
@@ -46,6 +29,29 @@ namespace BookCatalogApp.BookEntities
                 throw new ArgumentException("ISBN number already in catalog.");
             }
            
+        }
+
+        // Formats input string to 13 digit ISBN string.
+        public string FormatISBN(string iSBN)
+        {
+            var newISBN = String.Empty;
+            var digitsOnly = new Regex(@"^\d{13}$");
+            var digitsWithHyphens = new Regex(@"^\d{3}-\d-\d{2}-\d{6}-\d$");
+            var matchDigitsOnly = digitsOnly.Match(iSBN);
+            var matchDigitsWithHyphens = digitsWithHyphens.Match(iSBN);
+
+            if (matchDigitsOnly.Success)
+            {
+                return matchDigitsOnly.Value;
+            }
+            else if (matchDigitsWithHyphens.Success)
+            {
+                return matchDigitsWithHyphens.Value.Replace("-", String.Empty);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid ISBN number format.");
+            }            
         }
     }
 }
