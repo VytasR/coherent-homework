0f0ae7636e3b5c0df521f1ddc33c68b0aa25a023
VytasR <vytarink@gmail.com>
2022-01-26 10:56:48 +0200

    HW5, VacationsApp, class Company created

---
+++ Homework5/VacationsApp/CompanyEntities/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsApp.CompanyEntities
+{
+    internal class Company
+    {
+        private List<Vacation> _vacations;
+
+        public Company()
+        {
+            _vacations = new List<Vacation>();
+        }
+    }
+}
