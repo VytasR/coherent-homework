ba377606403d42d8ef68f4b09e67ddd911e453f4
VytasR <vytarink@gmail.com>
2022-01-26 10:48:11 +0200

    HW5, VacationsApp, class Vacation created

---
+++ Homework5/VacationsApp/CompanyEntities/Vacation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsApp.CompanyEntities
+{
+    internal class Vacation
+    {
+        public string EmployeeName { get; }
+        public DateTime FirstDay { get; }
+        public DateTime LastDay { get; }
+    }
+}
