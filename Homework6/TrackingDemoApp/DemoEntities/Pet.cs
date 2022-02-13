02f1a78108b49e8c0f0fcb40db962a9b9edac5ff
VytasR <vytarink@gmail.com>
2022-02-13 15:30:03 +0200

    HW6, TrackingDemoApp, class Pet modified

--- Homework6/TrackingDemoApp/DemoEntities/Pet.cs
+++ Homework6/TrackingDemoApp/DemoEntities/Pet.cs
@@ -8,6 +8,7 @@ namespace TrackingDemoApp.DemoEntities
 {
     public class Pet
     {
+        public string Type { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
     }
