0663b3a198e77429a8d53df1971541b08d2df928
VytasR <vytarink@gmail.com>
2022-02-13 12:33:41 +0200

    HW6, TrackingLib, class TrackingEntityAttribute created

---
+++ Homework6/TrackingLib/TrackingEntityAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingLib
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    internal class TrackingEntityAttribute : System.Attribute
+    {
+    }
+}
