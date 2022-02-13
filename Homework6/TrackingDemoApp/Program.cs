b6fdc7379039d4f4193dfc5eb63646c82b475f60
VytasR <vytarink@gmail.com>
2022-02-13 15:39:45 +0200

    HW6, TrackingDemoApp, demo modified in Program Main()

--- Homework6/TrackingDemoApp/Program.cs
+++ Homework6/TrackingDemoApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackingDemoApp.DemoEntities;
 
 namespace TrackingDemoApp
 {
@@ -10,6 +11,11 @@ namespace TrackingDemoApp
     {
         static void Main(string[] args)
         {
+            var addres = new Address() { StreetName = "5 Avenue", HouseNumber = "295", ZipCode = "10016-7103" };
+            var pets = new Pet[] { new Pet() { Type = "dog", Name = "Daisy", Age = 5 }, 
+                                   new Pet() { Type = "cat", Name = "Max", Age = 7 } };
+            var person = new Person() { FirstName = "Leonardo", LastName = "Green", Age = 35, Address = addres, _pets = pets };
+
         }
     }
 }
