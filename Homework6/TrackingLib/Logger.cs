d87bac3737f4ccf3f07b646ae67578809f5bd2f8
VytasR <vytarink@gmail.com>
2022-02-13 12:58:28 +0200

    HW6, TrackingLib, class Logger created

---
+++ Homework6/TrackingLib/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingLib
+{
+    internal class Logger
+    {
+        public string FileName { get; }
+
+        public Logger(string fileName)
+        {
+            if (String.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("File name can not be null or empty.");
+            }
+            FileName = fileName;
+        }
+    }
+}
