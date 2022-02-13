ae52711d31724819bf759611e4828c1646563a9f
VytasR <vytarink@gmail.com>
2022-02-13 15:20:27 +0200

    HW6, TrackingDemoApp, class Person modified

--- Homework6/TrackingDemoApp/DemoEntities/Person.cs
+++ Homework6/TrackingDemoApp/DemoEntities/Person.cs
@@ -7,7 +7,21 @@ using TrackingLib;
 
 namespace TrackingDemoApp.DemoEntities
 {    
+    [TrackingEntity]
     public class Person
-    {        
+    {
+        [TrackingProperty("First name")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [TrackingProperty]
+        public int Age { get; set; }
+
+        [TrackingProperty]
+        public Address Address { get; set; }
+
+        [TrackingProperty("Pets")]
+        public Pet[] _pets;
     }
 }
